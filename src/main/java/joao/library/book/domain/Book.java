47ab918f9096d9516ad72972636f16200be3efc3
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-04 15:09:17 -0300

    feat: adds a method to show attributes

--- src/main/java/joao/library/book/domain/Book.java
+++ src/main/java/joao/library/book/domain/Book.java
@@ -5,4 +5,11 @@ public class Book {
     String description;
     double price;
     String isbn;
+
+    void showDetails() {
+        System.out.println(name);
+        System.out.println(description);
+        System.out.println(price);
+        System.out.println(isbn);
+    }
 }
