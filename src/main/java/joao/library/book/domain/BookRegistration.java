227d3b0e60e8566d1590540b0b85f0c3339479c6
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-04 16:02:29 -0300

    feat: instantiate Author class

--- src/main/java/joao/library/book/domain/BookRegistration.java
+++ src/main/java/joao/library/book/domain/BookRegistration.java
@@ -1,22 +1,34 @@
 package main.java.joao.library.book.domain;
 
+import main.java.joao.library.author.domain.Author;
+
 public class BookRegistration {
     static void main(String[] args) {
-        Book book = new Book();
+        Author author = new Author();
+        author.name = "Rodrigo Turini";
+        author.email = "rodrigo.turini@caelum.com.br";
+        author.cpf = "123.456.789-10";
 
+        Book book = new Book();
         book.name = "Java 8 prático";
         book.description = "Novos recursos da linguagem";
         book.price = 59.90;
         book.isbn = "978-85-66258-46-6";
+        book.author = author;
 
         book.showDetails();
 
-        Book otherBook = new Book();
+        Author otherAuthor = new Author();
+        otherAuthor.name = "Paulo Silveira";
+        otherAuthor.email = "paulo.silveira@caelum.com.br";
+        otherAuthor.cpf = "123.456.789-10";
 
+        Book otherBook = new Book();
         otherBook.name = "Lógica de Programação";
         otherBook.description = "Crie os seus Primeiros Programas";
         otherBook.price = 59.90;
         otherBook.isbn = "978-85-66250-22-0";
+        otherBook.author = otherAuthor;
 
         otherBook.showDetails();
 
