fea496a562f1a1538a48501511342a1af943d73d
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-07 15:30:29 -0300

    fix: fixes access to attribute

--- src/main/java/joao/library/book/console/BookRegistration.java
+++ src/main/java/joao/library/book/console/BookRegistration.java
@@ -13,7 +13,7 @@ public class BookRegistration {
         Book book = new Book();
         book.name = "Java 8 prático";
         book.description = "Novos recursos da linguagem";
-        book.price = 59.90;
+        book.addPrice(59.90);
         book.isbn = "978-85-66258-46-6";
         book.author = author;
 
@@ -27,7 +27,7 @@ public class BookRegistration {
         Book otherBook = new Book();
         otherBook.name = "Lógica de Programação";
         otherBook.description = "Crie os seus Primeiros Programas";
-        otherBook.price = 59.90;
+        otherBook.addPrice(59.90);
         otherBook.isbn = "978-85-66250-22-0";
         otherBook.author = otherAuthor;
 
