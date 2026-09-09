97a31490317f97feaab3a955c6a298113107f09c
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-09 14:37:57 -0300

    fix: fixes call

--- src/main/java/joao/library/book/console/DiscountRules.java
+++ src/main/java/joao/library/book/console/DiscountRules.java
@@ -1,11 +1,15 @@
 package main.java.joao.library.book.console;
 
+import main.java.joao.library.author.domain.Author;
 import main.java.joao.library.book.domain.Book;
+import main.java.joao.library.fisicbook.domain.FisicBook;
 
 public class DiscountRules {
 
     static void main(String[] args) {
-        Book book = new Book();
+        Author author = new Author();
+        author.setName("Rodrigo Turini");
+        Book book = new FisicBook(author);
         book.addPrice(59.90);
 
         System.out.println("Valor atual(R$): " + book.getPrice());
