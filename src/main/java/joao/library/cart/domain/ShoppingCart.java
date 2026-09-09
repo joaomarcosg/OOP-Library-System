9a0abffee507257915d8e5aa8f68969c6074ff8a
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-09 14:15:00 -0300

    feat: adds an attribute to accumulate values and apply discount before

--- src/main/java/joao/library/cart/domain/ShoppingCart.java
+++ src/main/java/joao/library/cart/domain/ShoppingCart.java
@@ -3,9 +3,14 @@ package main.java.joao.library.cart.domain;
 import main.java.joao.library.book.domain.Book;
 
 public class ShoppingCart {
+    private double total;
 
     public void add(Book book) {
         System.out.println("Adding " + book);
+        book.applyDiscount(0.05);
+        total += book.getPrice();
     }
+
+    public double getTotal() {return total;}
 
 }
