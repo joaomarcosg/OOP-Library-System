506747c73a1eeccf50daa53b75a50549b7141c40
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-09 14:53:54 -0300

    feat: adds a new type of book

---
+++ src/main/java/joao/library/minibook/domain/MiniBook.java
@@ -0,0 +1,10 @@
+package main.java.joao.library.minibook.domain;
+
+import main.java.joao.library.author.domain.Author;
+import main.java.joao.library.book.domain.Book;
+
+public class MiniBook extends Book {
+    public MiniBook(Author author) {
+        super(author);
+    }
+}
