0f40e16003d33cec29126f73084b38cf29e9481e
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-10 14:52:38 -0300

    refactor: changes type parameter to receive an interface

--- src/main/java/joao/library/cart/domain/ShoppingCart.java
+++ src/main/java/joao/library/cart/domain/ShoppingCart.java
@@ -1,14 +1,14 @@
 package main.java.joao.library.cart.domain;
 
 import main.java.joao.library.book.domain.Book;
+import main.java.joao.library.product.domain.Product;
 
 public class ShoppingCart {
     private double total;
 
-    public void add(Book book) {
-        System.out.println("Adding " + book);
-        book.applyDiscount(0.05);
-        total += book.getPrice();
+    public void add(Product product) {
+        System.out.println("Adding " + product);
+        total += product.getPrice();
     }
 
     public double getTotal() {return total;}
