b157acce6a6ddca73c312b0264f2e94ffe496f3e
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-10 14:25:50 -0300

    feat: adds a type Magazine

---
+++ src/main/java/joao/library/magazine/domain/Magazine.java
@@ -0,0 +1,49 @@
+package main.java.joao.library.magazine.domain;
+
+public class Magazine {
+    private String name;
+    private String description;
+    private double price;
+    private Publisher publisher;
+
+    public boolean applyDiscount(double percent) {
+        if (percent > 0.1) {
+            return false;
+        }
+        double discount = this.getPrice() * percent;
+        this.setPrice(this.getPrice() - discount);
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+}
