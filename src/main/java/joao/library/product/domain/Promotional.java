47733da7e9dd6d7fa1bb562f2b26dabdea807c0c
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-11 14:03:14 -0300

    feat: adds a concrete method in the interface

--- src/main/java/joao/library/product/domain/Promotional.java
+++ src/main/java/joao/library/product/domain/Promotional.java
@@ -1,5 +1,11 @@
 package main.java.joao.library.product.domain;
 
 public interface Promotional {
+
     boolean applyDiscount(double percent);
+
+    default boolean applyDiscountOf10Percent() {
+        return applyDiscount(0.1);
+    }
+
 }
